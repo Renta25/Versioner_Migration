690f0664f574964c144db155880f6a61075873fa
Arkadiusz Bryska <arkadiusz.bryska@gmail.com>
2020-12-23 18:34:55 +0100

    Create softwares

---
+++ 20201223181252_create_software.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS softwares(
+    id  INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name TEXT NOT NULL,
+    private BOOLEAN NOT NULL,
+    creator_id INT UNSIGNED NOT NULL,
+    create_data DATETIME NOT NULL DEFAULT NOW(),
+    modifer_id INT UNSIGNED,
+    modify_data DATETIME DEFAULT NULL ON UPDATE NOW(),  
+    FOREIGN KEY (creator_id) REFERENCES accounts (id)
+);
