f5e836e35c0b03a833d300aa78d2fe2d70d2f246
Arkadiusz Bryska <arkadiusz.bryska@gmail.com>
2020-12-24 16:33:16 +0100

    ADD Create software_system + modify (add column system_id FK) + modify project file

--- 20201224120522_create_software_system.up.sql
+++ 20201224120522_create_software_system.up.sql
@@ -1,24 +1,13 @@
-CREATE TABLE IF NOT EXISTS versions(
+CREATE TABLE IF NOT EXISTS software_system(
     id  INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     software_id  INT UNSIGNED NOT NULL,
-    system_id  INT UNSIGNED NOT NULL,
     name TEXT NOT NULL,
-    build INT UNSIGNED NOT NULL,
-    hash TEXT NOT NULL,
-    size INT NOT NULL,
-    public BOOLEAN NOT NULL DEFAULT FALSE,
-    public_data DATETIME NOT NULL DEFAULT NOW(),
-    recall BOOLEAN NOT NULL DEFAULT FALSE,
-    recall_data DATETIME NOT NULL DEFAULT NOW(),
     creator_id INT UNSIGNED,
     create_data DATETIME NOT NULL DEFAULT NOW(),
     modifer_id INT UNSIGNED,
     modify_data DATETIME DEFAULT NULL ON UPDATE NOW(),
 
     FOREIGN KEY (software_id) REFERENCES softwares (id)
-        ON DELETE CASCADE,
-    
-    FOREIGN KEY (system_id) REFERENCES software_system (id)
         ON DELETE CASCADE,
 
     FOREIGN KEY (creator_id) REFERENCES accounts (id)
